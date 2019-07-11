d8b24582c52694068d7034ceeaeb4cfcaa8dc253
Nepcam <cameron.nepe@gmail.com>
2019-07-11 22:16:56 +1200

    GET method sorted

--- easyhttp.js
+++ easyhttp.js
@@ -3,8 +3,19 @@ function easyHTTP() {
 }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url) {
-  
+easyHTTP.prototype.get = function(url, callback) {
+  this.http.open('GET', url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  }
+
+  this.http.send()
 }
 // Make an HTTP POST Request
 
