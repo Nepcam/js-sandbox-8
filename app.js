877dc7dc9b4c753366afc123b40dc3a8ced80e04
Nepcam <cameron.nepe@gmail.com>
2019-07-12 18:24:12 +1200

    POST, PUT, DELETE Request

--- app.js
+++ app.js
@@ -25,10 +25,28 @@ const data = {
 };
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
-  if(err) {
-        console.log(err);
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else { 
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+    if(err) {
+    console.log(err);
   } else { 
     console.log(post);
+  }
+});
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else { 
+console.log(posts);
   }
 });
